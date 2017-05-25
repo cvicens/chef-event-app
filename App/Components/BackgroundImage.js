4dac14b28dc626b65c5ca096c7953427d38e93dc
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2017-05-25 19:45:49 +0200

    version alfa

---
+++ App/Components/BackgroundImage.js
@@ -0,0 +1,32 @@
+import React, { PropTypes } from 'react'
+import { Image, StyleSheet } from 'react-native'
+
+export default class BackgroundImage extends React.Component {
+    static propTypes = {
+        image: PropTypes.number,
+        resizeMode: PropTypes.string
+    }
+
+    _styles = StyleSheet.create({
+        backgroundImage: {
+            flex: 1,
+            width: null,
+            height: null
+        }
+    })
+
+    render() {
+        const __style = [ this._styles.backgroundImage,
+            { resizeMode: this.props.resizeMode || 'cover' }
+        ]
+        return (
+            <Image 
+                source={this.props.image}
+                style={__style}>
+
+                  {this.props.children}
+
+            </Image>
+        )
+    }
+}
